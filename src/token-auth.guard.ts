183b8b2ff33258ff7e828769c19dfc3f126ee7ff
mahaaaya1-source <mahaaaya1@gmail.com>
2026-04-28 12:34:33 +0600

    add token and admin guards

---
+++ src/token-auth.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class TokenAuthGuard implements CanActivate {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.get('Authorization');
+
+    if (!token) {
+      return false;
+    }
+
+    const user = await this.userModel.findOne({ token });
+
+    if (!user) {
+      return false;
+    }
+
+    request.user = user;
+
+    return true;
+  }
+}

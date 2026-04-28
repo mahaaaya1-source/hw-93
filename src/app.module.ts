1dc1750a33a5ded6eb43508e77fa5f45414a9516
mahaaaya1-source <mahaaaya1@gmail.com>
2026-04-28 12:36:40 +0600

    register user model and auth providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,37 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 
 import { ArtistsController } from './artists/artists.controller';
 import { AlbumsController } from './albums/albums.controller';
 import { TracksController } from './tracks/tracks.controller';
+import { UsersController } from './users/users.controller';
 
 import { Artist, ArtistSchema } from './schemas/artist.schema';
 import { Album, AlbumSchema } from './schemas/album.schema';
 import { Track, TrackSchema } from './schemas/track.schema';
+import { User, UserSchema } from './schemas/user.schema';
+
+import { AuthService } from './auth/auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     MongooseModule.forRoot('mongodb://localhost/music-api-nest-93'),
     MongooseModule.forFeature([
       { name: Artist.name, schema: ArtistSchema },
       { name: Album.name, schema: AlbumSchema },
       { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
-  controllers: [ArtistsController, AlbumsController, TracksController],
+  controllers: [
+    ArtistsController,
+    AlbumsController,
+    TracksController,
+    UsersController,
+  ],
+  providers: [AuthService, LocalStrategy],
 })
 export class AppModule {}
